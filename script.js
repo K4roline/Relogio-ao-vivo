78ba6b8c799708e26cdd8911d0e9a3a2a40124d8
Karoline Correa <ckaroline226@gmail.com>
2022-07-19 22:10:13 -0300

    Corrigindo a funcionalidade e atualizando o estilo

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-function mostrarrelogio() {
-    var data = new Date()
-    var data = date.getHours()
-    var data = date.getMinutes()
-    var data = date.getSeconds()
-    var sessao = "AM"
+function relogio() {
+    var Data = new Date()
+    var hora = Data.getHours()
+    var minutos = Data.getMinutes()
+    var segundos = Data.getSeconds()
 
-    if (hora == 0) {
-        hora = 12
-    }
-
-    if (hora > 12) {
-        hora = hora - 12; 
-        sessao = "PM"
-    }
-
-    hora = (hora < 10) ? "0" + hora : hora;
-    minutos = (minutos < 10) ? "0" + minutos : minutos;
-    segundos = (segundos < 10) ? "0" + segundos : segundos;
-
-    var time = hora + ":" + minutos + ":" + segundos + " " + sessao;
-    document.getElementById("relogio").innerHTML = time;
-    document.getElementById("relogio").textContent = time;
-
-    setTimeout(mostrartempo, 1000);
+    hora = (hora < 10) ? "0" + hora : hora
+    minutos = (minutos < 10) ? "0" + minutos : minutos
+    segundos = (segundos < 10) ? "0" + segundos : segundos
     
-}
+    var tempo = hora + ":" + minutos + ":" + segundos
 
-mostrarrelogio()
+    tempo.innerHTMl = tempo
+
+    document.getElementById("relogio").value = tempo
+}
+    setInterval(relogio, 500)
+
+
+    
