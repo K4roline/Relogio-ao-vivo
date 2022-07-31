924519fa04e8cc5dcd63536f611c3e9bce3b014b
Karoline Correa <ckaroline226@gmail.com>
2022-07-31 19:01:29 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -14,31 +14,24 @@ function relogio() {
 
     document.getElementById("relogio").value = tempo
 }
-    setInterval(relogio, 500)
 
 function carregarimagem() {
-    var img = window.document.getElementById('imagemdia')    
-    var img = window.document.getElementById('imagemtarde')   
-    var img = window.document.getElementById('imagemnoite')   
+
+    var img = window.document.getElementById('imagemdia')
 
     var data = new Date()
     var hora = data.getHours()
 
     if (hora >= 0 && hora < 12) {
-        img.innerHTML = '<img src="manha.jpg">'
-    } else if (hora >= 12 && hora <= 18) {
-        img.innerHTML = '<img src="tarde.jpg">'
+        img.setAttribute("src", "manha.jpg")
+    } else if (hora >= 12 && hora < 18) {
+        img.setAttribute("src", "tarde.jpg")
     } else {
-        img.innerHTML = '<img src="noite.jpg">'
+        img.setAttribute("src", "noite.jpg")
     }
-
 }
 
-
-// if (hora >= 0 && hora < 12) {
-//     img.scr = 'manha.jpg'
-// } else if (hora >= 12 && hora <= 18) {
-//     img.scr = 'tarde.jpg'
-// } else {
-//     img.scr = 'noite.jpg'
-// }
+function inicio() {
+    relogio()
+    carregarimagem()
+}
